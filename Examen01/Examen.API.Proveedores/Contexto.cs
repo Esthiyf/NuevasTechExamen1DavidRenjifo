8cc4132c4d164d9d71105a8d220cd68a040c82a8
david esthyf renjifo flores <tj.david.renjifo.f@upds.net.bo>
2024-03-20 23:16:38 -0400

    Examen Primer

---
+++ Examen01/Examen.API.Proveedores/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Coling.Shared;
+
+namespace Examen.API.Proveedores
+{
+        public class Contexto : DbContext
+        {
+            public Contexto(DbContextOptions<Contexto> options) : base(options)
+            {
+
+            }
+            public virtual DbSet<Proveedor> Proveedores { get; set; }
+        }
+    }
